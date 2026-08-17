40404a062d10f8e8b69ab7c32c3c5ceafa06d8f7
Nicola Ferru <ask.nfvblog@outlook.it>
2026-08-17 10:11:55 +0200

    feat: add LaTeX-to-SVG conversion support via dvisvgm and update package URL

--- ox-epub3-pkg.el
+++ ox-epub3-pkg.el
@@ -8,6 +8,6 @@
   '((emacs "25.1")
     (org "9.0"))
   :keywords '("epub" "org-mode" "export" "ebook")
-  :url "https://github.com/NFVblog/ox-epub3"
+  :url "https://github.com/NF02/ox-epub3"
   :license "GPL-3.0")
 ;;; ox-epub3-pkg.el ends here
