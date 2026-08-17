b61b8300f3c690e8d25ec63bb4a63062dd436b4f
Nicola Ferru <ask.nfvblog@outlook.it>
2026-08-17 09:33:21 +0200

    add license

--- ox-epub3.el
+++ ox-epub3.el
@@ -6,14 +6,22 @@
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1") (org "9.0"))
 ;; Keywords: epub, org-mode, export, ebook
-;; URL: https://github.com/user/ox-epub3
+;; URL: https://github.com/NFVblog/ox-epub3
 
 ;; This file is not part of GNU Emacs.
 
-;; This program is free software; you can redistribute it and/or modify
+;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
